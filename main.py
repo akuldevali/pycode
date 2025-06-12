109cf9372c9abcca86af8c9d1261ab377a8d7399
Akul G Devali <64837282+akuldevali@users.noreply.github.com>
2025-06-12 02:21:35 +0000

    first chain test

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers upto 10")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
 
 
 llm = OpenAI(
     openai_api_key= api_key
 )
 
-result = llm("Who is the best cricket batsman in the world?")
-print(result)
+code_prompt = PromptTemplate(
+    template = "Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+    llm = llm,
+    prompt = code_prompt
+)
+
+result = code_chain({
+    "language": args.language,
+    "task": args.task
+})
+
+print(result["text"])
+
+# result = llm("Who is the best cricket batsman in the world?")
+# print(result)
